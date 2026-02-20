aa8d905d69be76a3fbc0e181b47c0ebcebe9cb77
abuzar-ali1 <abuzaralixwave@gmail.com>
2026-02-21 02:35:34 +0500

    made the views and urls in API Folder

--- djangoapi/API/views.py
+++ djangoapi/API/views.py
@@ -1,2 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+
+@api_view(['GET'])
+def getData():
+    person = {'name' : 'Abuzar Ali', 'age' : 19, 'stack' : 'Python Full stack Dev'}
+    return Response(person) 
